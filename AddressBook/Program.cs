e3e8d374a1d62d72427214f1be9a1ed7ad745040
Prerana <dahiwadeprerana@gmail.com>
2022-11-17 20:22:04 +0530

    [prerana]Add:CreateContact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace AddressBook
+﻿using AddressBook;
+
+namespace AddressBook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WelCome to AddressBook!");
+            Contact contact = new Contact()
+            {
+                FirstName = "Prerana",
+                LastName = "Dahiwade",
+                Address = "Karanja",
+                City = "Nagpur",
+                State = "Maharastra",
+                Zip = 442203,
+                PhoneNo = "8976542345",
+                Email = "preranad@gmail.com"
+
+            };
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNo + "\n" + contact.Email);
         }
     }
 }
