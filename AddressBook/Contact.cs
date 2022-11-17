880af12f366e952d2dc97fb4cfbc845aa9795da4
Prerana <dahiwadeprerana@gmail.com>
2022-11-17 20:46:38 +0530

    [prerana]Add:AddContacts

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBook
+namespace ADDRESSBOOK
 {
     internal class Contact
     {
@@ -16,5 +16,7 @@ namespace AddressBook
         public int Zip { get; set; }
         public string PhoneNo { get; set; }
         public string Email { get; set; }
+
+      
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,24 +1,53 @@
-﻿using AddressBook;
+﻿
 
-namespace AddressBook
+namespace ADDRESSBOOK
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Contact contact = new Contact()
+            bool flag = true;
+            while (flag)
             {
-                FirstName = "Prerana",
-                LastName = "Dahiwade",
-                Address = "Karanja",
-                City = "Nagpur",
-                State = "Maharastra",
-                Zip = 442203,
-                PhoneNo = "8976542345",
-                Email = "preranad@gmail.com"
+                Console.WriteLine("\nselect Program\n 1.AddContacts");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        AddressBook addressBook = new AddressBook();
+                        Contact contact = new Contact();
+                        {
+                            Console.WriteLine("Enter FirstName");
+                            contact.FirstName = Console.ReadLine();
+                            Console.WriteLine("Enter LastName");
+                            contact.LastName = Console.ReadLine();
+                            Console.WriteLine("Enter Address");
+                            contact.Address = Console.ReadLine();
+                            Console.WriteLine("Enter City");
+                            contact.City = Console.ReadLine();
+                            Console.WriteLine("Enter State");
+                            contact.State = Console.ReadLine();
+                            Console.WriteLine("Enter Zip Code");
+                            contact.Zip = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter PhoneNo");
+                            contact.PhoneNo = Console.ReadLine();
+                            Console.WriteLine("Enter Email");
+                            contact.Email = Console.ReadLine();
+                        }
+                        Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNo + "\n" + contact.Email);
+                        addressBook.AddContact(contact);
+                        break;
 
-            };
-            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNo + "\n" + contact.Email);
+
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+
+
+            }
+
+
         }
     }
 }
